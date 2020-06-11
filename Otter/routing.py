18562fe00f08d1d0a1fe29c69100eaa7e895f59a
Bluefissure <bluefissure@foxmail.com>
2020-06-11 00:41:33 +0000

    rename app

--- Otter/routing.py
+++ Otter/routing.py
@@ -2,12 +2,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
 import ffxivbot.consumers as consumers
 from django.conf.urls import url
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter([
-            # url(r'^ws_api', consumers.APIConsumer),
-            # url(r'^ws_event', consumers.EventConsumer),
             url(r'^ws(?!_)', consumers.WSConsumer),
             ])
     )
